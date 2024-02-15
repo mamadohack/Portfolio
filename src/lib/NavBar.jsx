38cb5d5a87630fb5c929870d0671e0787db2553d
unknown <mamadohack@gmail.com>
2024-02-15 18:11:32 +0100

    change home tag text color

--- src/lib/NavBar.jsx
+++ src/lib/NavBar.jsx
@@ -84,7 +84,7 @@ const NavBar = ({}) => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  className="block py-2 px-3 text-white bg-blue-900 rounded md:bg-transparent md:p-0 text-white hover:text-blue-700"
                   aria-current="page"
                 >
                   Home
